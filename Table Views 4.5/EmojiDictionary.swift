4f0ed01ac0f0f8b2a5f1b688d5972314ca4199e4
Anna Lopez <annamlopez8@gmail.com>
2019-03-16 10:46:11 -0500

    4.5 project table view

---
+++ Table Views 4.5/EmojiDictionary.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  Table Views 4.5
+//
+//  Created by Anna CS on 3/14/19.
+//  Copyright © 2019 Anna Lopez and Salvador Garcia. All rights reserved.
+//
+
+import Foundation
+
+class Emoji{
+    var symbol:String
+    var name:String
+    var description:String
+    var usage:String
+    init (symbol:String, name:String, description:String, usage:String){
+        self.symbol = symbol
+        self.name = name
+        self.description = description
+        self.usage = usage
+        
+       
+        
+        
+    }
+
+}
+
